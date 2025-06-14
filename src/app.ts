b4ae03946f99d9781fae2a98361faa858df3f2b8
Nataliia Pohorenko <natasha.pogorenko@gmail.com>
2025-06-14 12:38:58 +0300

    updated cors policy

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,19 @@ import { prisma } from './prisma/client';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://superhero-app-liqt.onrender.com',
+  credentials: true,
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use('/api/heroes', heroRoutes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+const PORT = Number(process.env.PORT) || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
+});
 process.on('SIGINT', async () => {
   await prisma.$disconnect();
   process.exit(0);
